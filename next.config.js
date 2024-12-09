8b25f79eabbe71e11510a08ce967e1ebbf217128
morizkay <morizkay@live.com>
2024-12-10 06:43:39 +0700

    feat: Add GitHub Pages deployment configuration

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/baparkir-website',
+}
+
+module.exports = nextConfig
